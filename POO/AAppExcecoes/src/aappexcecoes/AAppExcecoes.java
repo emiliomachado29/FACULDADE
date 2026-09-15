7844bdef7476586b7938cd6e1b1e740d76f5de17
Emílio <emiliodasilvamachado@gmail.com>
2026-09-15 10:15:27 -0300

    Add files via upload

---
+++ POO/AAppExcecoes/src/aappexcecoes/AAppExcecoes.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aappexcecoes;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author emili
+ */
+public class AAppExcecoes {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Iniciando a Main");
+        Exceptions exemplo = new Exceptions();
+        exemplo.lerArquivoTXT();
+        //try {
+            //exemplo.throws01();
+        //} catch (Exception ex) {
+           // System.out.println("Formato de idade inválida!");
+        //}
+       // System.out.println("Saindo da Main");
+    }   
+}
